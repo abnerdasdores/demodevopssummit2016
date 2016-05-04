8c3e29a267d70dde9fb7669eaad155d9e3692349
Abner das Dores <abner.dores@lambda3.com.br>
2016-05-04 13:12:17 -0300

    Adicionando SDK do Application Insights

--- src/DemoAppInsights/Global.asax.cs
+++ src/DemoAppInsights/Global.asax.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using Microsoft.ApplicationInsights.Extensibility;
+using System.Configuration;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -13,6 +11,7 @@ namespace DemoAppInsights
     {
         protected void Application_Start()
         {
+            TelemetryConfiguration.Active.InstrumentationKey = ConfigurationManager.AppSettings["ikey"];
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
